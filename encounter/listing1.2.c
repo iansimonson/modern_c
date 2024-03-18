00233ea5cd2aba30042667afc5419984eeddabd3
Ian Simonson <ian.simonson@protonmail.com>
2024-03-17 19:18:36 -0700

    Encounter done

---
+++ encounter/listing1.2.c
@@ -0,0 +1,18 @@
+/* Does not compile */
+#include <stdlib.h>
+
+void main() {
+  int i;
+  double A[5] = {
+      [0] = 9.0,
+      [1] = 2.9,
+      [4] = 3.E+25,
+      [3] = .0007,
+  };
+
+  for (i = 0; i < 5; ++i) {
+    printf("element %zu is %g, \tits square is %g\n", i, A[i], A[i] * A[i]);
+  }
+
+  return 0;
+}
